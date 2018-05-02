fcffa3637fa2209ed3eaba6dda32e4507dd22f65
Chu Hung Cheng <chccheng@ucdavis.edu>
2018-05-02 10:16:35 -0700

    DDPG draft

---
+++ ddpg.py
@@ -0,0 +1,41 @@
+#### Actor
+# __init__
+# _build_net
+# learn
+# choose action
+class Actor():
+    def __init__():
+
+    def build_actor():
+
+    def learn():
+
+    def choose_action():
+
+#### Critic
+# __init__
+# _build_net
+# learn
+class Critic():
+    def __init__():
+
+    def build_critic():
+
+    def learn():
+
+#### Memory
+
+class Memory():
+    # __init__
+    def __init__():
+
+    # store_transitions
+    def store_transitions():
+
+    # randomize_memory
+    def randomize_memory():
+
+#### initialization + sess
+
+
+#### environment
